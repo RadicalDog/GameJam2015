f2bbe1c7048c9af8678dbfc375ab728117be37c3
Thomas Knight <atmhso@gmail.com>
2015-10-31 16:22:47 +0000

    akjdnfkjsandfkljsandflkjsdnf

--- Super Cool Robot Fighting/Assets/Scripts/RobotController.cs
+++ Super Cool Robot Fighting/Assets/Scripts/RobotController.cs
@@ -6,32 +6,33 @@ public class RobotController : MonoBehaviour {
 
 	// Contains data to be used in building and resetting robots
 
-    List<solids> DataSolids = new List<solids>();
-    List<joints> DataJoints = new List<joints>();
+    public List<solids> DataSolids = new List<solids>();
+    public List<joints> DataJoints = new List<joints>();
 
     public GameObject Rectangle;
     public GameObject Circle;
 
 	void Start () {
         Debug.Log("adasdasds");
-        solids crappySolid = new solids();
-        DataSolids.Add(crappySolid);
-        DataSolids.Add(crappySolid);
-        DataSolids.Add(crappySolid);
+        for (int i = 0; i < 3; i++)
+        {
+            solids crappySolid = new solids();
+            DataSolids.Add(crappySolid);
+        }
         Debug.Log(DataSolids);
+
+        joints simple = new joints();
+        DataJoints.Add(simple);
+
+        DataSolids[0].x = 1;
+        DataSolids[2].x = -1;
 
         updateRobot();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-        //debugging without showing a billion messages
-        if (Random.value < 0.02)
-        {
-            updateRobot();
-        }
-        
+                
 	}
 
     void updateRobot()
@@ -40,18 +41,24 @@ public class RobotController : MonoBehaviour {
         Debug.Log("starting update");
         DataSolids.ForEach(solidBuild);
         DataJoints.ForEach(jointBuild);
+
     }
 
     private void jointBuild(joints obj)
     {
         //build the joints
+        obj.h = DataSolids[1].o.AddComponent<HingeJoint2D>();
+        obj.h.connectedBody = DataSolids[0].o.GetComponent<Rigidbody2D>();
+        Debug.Log("hinge "+obj.h);
+            //.rigidbody2D;
     }
 
     private void solidBuild(solids obj)
     {
         //Build a solid!
-        obj.visual = Instantiate(Rectangle);
-
+        obj.o = Instantiate(Rectangle);
+        obj.o.transform.parent = transform;
+        obj.o.GetComponent<RectangleScript>().rebuild(obj.width, obj.height, obj.rotation, obj.x, obj.y);
     }
 
     void deleteRobot()
@@ -62,7 +69,7 @@ public class RobotController : MonoBehaviour {
 
     private void deleteThisSolid(solids obj)
     {
-        Destroy(obj.visual);
+        Destroy(obj.o);
     }
     private void deleteThisJoint(joints obj)
     {
@@ -72,18 +79,19 @@ public class RobotController : MonoBehaviour {
 
 public class solids
 {
-    float height = 10;
-    float width = 10;
-    float rotation = 0;
-    float x = 0;
-    float y = 0;
-    public GameObject visual;
-    int type = 1;
+    public int height = 3;
+    public int width = 3;
+    public int rotation = 0;
+    public float x = 0;
+    public float y = 4;
+    public GameObject o;
+    public int type = 1;
 }
 
 public class joints
 {
-    int type = 1;
-    int rootConnection;
-    int targConnection;
+    public int type = 1;
+    public int rootConnection;
+    public int targConnection;
+    public HingeJoint2D h;
 }
